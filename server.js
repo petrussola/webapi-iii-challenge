746928d7ff090985c391b5d5bf5c7b3c8086fbc2
Pere <pere.solaclaver@gmail.com>
2019-10-30 12:25:21 +0000

    Import express, helmet and cors. Use them

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = 'express';
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const server = express();
+server.use(helmet());
+server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
