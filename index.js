fdbacc8dad030dbcd4e4d963fae4bebf244d0975
Pere <pere.solaclaver@gmail.com>
2019-10-31 13:49:58 +0000

    Wire stuff for heroku deploy

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // code away!
 
+require('dotenv').config();
+
+const port = process.env.PORT;
+
 const server = require("./server");
 
 // SERVER LISTENING
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log("\n* Server Running on http://localhost:3000 *\n");
 });
